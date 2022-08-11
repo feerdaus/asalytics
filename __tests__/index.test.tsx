c3d5d4b2a389e3cdcfa1cb10873fa112e43eaaec
feerdaus <firdauslawal@gmail.com>
2022-08-11 23:34:45 +0100

    Add ASACard test

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,6 +1,13 @@
+import ASACard from "@components/ASACard";
 import Header from "@components/Header";
 import Home from "@pages/index";
 import { render, screen } from "@testing-library/react";
+
+const mockData = {
+  name: "PLANET",
+  available: false,
+  logo: "https://algorand-wallet-mainnet.s3.amazonaws.com/media/asset_verification_requests_logo_svg/2022/06/10/6b08601da44c4f72bc71a007a0781f34.svg",
+};
 
 describe("Home", () => {
   it("renders home", () => {
@@ -19,5 +26,18 @@ describe("Home", () => {
     const nav = screen.getByTestId("nav");
     expect(heading).toBeInTheDocument();
     expect(nav).toBeInTheDocument();
+  });
+
+  it("renders ASAlist", () => {
+    render(
+      <ASACard
+        name={mockData.name}
+        logo={mockData.logo}
+        available={mockData.available}
+      />
+    );
+
+    const logo = screen.getByAltText(`${mockData.name} logo`);
+    expect(logo).toBeInTheDocument();
   });
 });
