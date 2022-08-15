da094bdd9f78e8deb41197aa99aec5d9aefe2761
feerdaus <firdauslawal@gmail.com>
2022-08-15 11:24:36 +0100

    Switch back logo to svg component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,19 +1,13 @@
 import { FC } from "react";
 import styles from "@styles/Home.module.css";
-import Image from "next/image";
+import Logo from "./Logo";
 
 const Header: FC = () => {
   return (
     <header>
       <nav data-testid="nav" className="nav">
         <div className={styles.logo}>
-          <Image
-            src="https://ik.imagekit.io/4woq8zlkh/Asalytics/Header_Logo_PbOOcENNS.svg?ik-sdk-version=javascript-1.4.3&updatedAt=1660291324200"
-            alt="Asalytics logo"
-            width={152}
-            height={71}
-            layout="responsive"
-          />
+          <Logo />
         </div>
         <a href="#" className="fs-16-20 bg-black fw-400 txt-white btn">
           ANALYZE ASAs
